02a5688a2ef031437733bc81096e7694a752ad30
Victoria Nunez <victorianunez@Victorianunez.local>
2020-12-27 13:34:31 +0100

    documentation and refactoring

--- src/Chess/Board.cs
+++ src/Chess/Board.cs
@@ -10,10 +10,10 @@ namespace Chess
     /// Bitboards are longs. Let a bitboard be a string of bits indexed from 0 to 64, then 
     /// the graphic rep of the board is:
     ///
-    /// 07 06 05 04 03 02 01 00  
-    /// 15 14 13 12 11 10 09 08
+    /// 00 01 02 03 04 05 06 07  
+    /// 08 09 10 11 12 13 14 15
     /// ...
-    /// 63 62 61 60 59 58 57 56
+    /// 56 57 58 59 60 61 62 63
     /// </summary>
     public class Board
     {
@@ -29,8 +29,10 @@ namespace Chess
         public Bishop BB = new Bishop(false, 0L);
         public Knight BN = new Knight(false, 0L);
         public Pawn BP = new Pawn(false, 0L);
-        public static string[,] chessBoard = new string[8,8];
-        private static string stringBoard;
+        /// <summary>
+        /// Array of chars to provide graphical rep of the board
+        /// </summary>
+        public static string[,] charBoard = new string[8,8];
         private static int stringLengthDif;
 
         /// <summary>
@@ -38,7 +40,7 @@ namespace Chess
         /// </summary>
         public Board()
         {
-            char[,] initialChessBoard = {
+            char[,] initialcharBoard = {
                 {'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                 {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                 {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -48,108 +50,106 @@ namespace Chess
                 {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                 {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}  
             };
-            arrayToBitboards(initialChessBoard);
-
+            this.charBoard = initialCharBoard;
+            arrayToBitboards();
         }
 
         /// <summary>
-        /// <param name="chessBoard"> Array of chars in starting position in graphic representation (as above). Uppercase indicate  white, lowercase black pieces.</param>
-        /// Method iterates through char array and adds a piece to the bitboard according to its graphic rep.
+        /// Iterates through char array and generates all the bitboards for each piece.
         /// </summary>
-        public void arrayToBitboards(char[,] chessBoard, King WK, Queen WQ, Rook WR, Knight WN, Bishop WB, Pawn WP, King BK, Queen BQ, Rook BR, Knight BN, Bishop BB, Pawn BP)
+        public void arrayToBitboards()
         {
             for(int i = 0; i < 64; i++)
             {
                 var bin = "0000000000000000000000000000000000000000000000000000000000000000"; 
                 bin = bin.Substring(i+1) + "1" + bin.Substring(0, i);
 
-                switch(chessBoard[i / 8, i % 8])
+                switch(this.charBoard[i / 8, i % 8])
                 {
-                    case 'r': BR.bb += convertStringToBitboard(bin);
+                    case 'r': this.BR.bb += convertStringToBitboard(bin);
                         break;
-                    case 'n': BN.bb += convertStringToBitboard(bin);
+                    case 'n': this.BN.bb += convertStringToBitboard(bin);
                         break;
-                    case 'b': BB.bb += convertStringToBitboard(bin);
+                    case 'b': this.BB.bb += convertStringToBitboard(bin);
                         break;
-                    case 'q': BQ.bb += convertStringToBitboard(bin);
+                    case 'q': this.BQ.bb += convertStringToBitboard(bin);
                         break;
-                    case 'k': BK.bb += convertStringToBitboard(bin);
+                    case 'k': this.BK.bb += convertStringToBitboard(bin);
                         break;
-                    case 'p': BP.bb += convertStringToBitboard(bin);
+                    case 'p': this.BP.bb += convertStringToBitboard(bin);
                         break;
-                    case 'R': WR.bb += convertStringToBitboard(bin);
+                    case 'R': this.WR.bb += convertStringToBitboard(bin);
                         break;
-                    case 'N': WN.bb += convertStringToBitboard(bin);
+                    case 'N': this.WN.bb += convertStringToBitboard(bin);
                         break;
-                    case 'B': WB.bb += convertStringToBitboard(bin);
+                    case 'B': this.WB.bb += convertStringToBitboard(bin);
                         break;
-                    case 'Q': WQ.bb += convertStringToBitboard(bin);
+                    case 'Q': this.WQ.bb += convertStringToBitboard(bin);
                         break;
-                    case 'K': WK.bb += convertStringToBitboard(bin);
+                    case 'K': this.WK.bb += convertStringToBitboard(bin);
                         break;
-                    case 'P': WP.bb += convertStringToBitboard(bin);
+                    case 'P': this.WP.bb += convertStringToBitboard(bin);
                         break;
                 }
             }
-            drawBoardArray(WK, WQ, WR, WN, WB, WP, BK, BQ, BR, BN, BB, BP);
+            this.drawBoardArray();
         }
 
         /// <summary>
         /// <param name="bin"> String of 0s and 1s of length 64.</param>
         /// Returns a long.
         /// </summary>
-        private long convertStringToBitboard(string bin)
+        private static long convertStringToBitboard(string bin)
         {
             return Convert.ToInt64(bin, 2);
         }
 
         /// <summary>
-        /// <param name="chessBoard"> Array of chars in starting position in graphic representation (as above). Uppercase indicate  white, lowercase black pieces.</param>
-        /// Takes pieces as params to initialize their bitboards (referred to as bb).
-        /// Method iterates through char array and adds a piece to the bitboard according to its graphic rep.
+        /// Converts a board (set of bitboards) into an array of chars for graphical rep
         /// </summary>
-        public static void drawBoardArray(King WK, Queen WQ, Rook WR, Knight WN, Bishop WB, Pawn WP, King BK, Queen BQ, Rook BR, Knight BN, Bishop BB, Pawn BP)
+        public void drawBoardArray()
         {
             for(int i = 0; i < 64; i++)
             {
-                if (((WK.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "K"; }
-                if (((WQ.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "Q"; } 
-                if (((WR.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "R"; } 
-                if (((WN.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "N"; } 
-                if (((WB.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "B"; } 
-                if (((WP.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "P"; } 
-                if (((BK.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "k"; } 
-                if (((BQ.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "q"; } 
-                if (((BR.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "r"; } 
-                if (((BN.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "n"; } 
-                if (((BB.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "b"; } 
-                if (((BP.bb>>i)&1)==1) { chessBoard[i / 8, i % 8] = "p"; } 
+                if (((WK.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "K"; }
+                if (((WQ.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "Q"; } 
+                if (((WR.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "R"; } 
+                if (((WN.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "N"; } 
+                if (((WB.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "B"; } 
+                if (((WP.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "P"; } 
+                if (((BK.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "k"; } 
+                if (((BQ.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "q"; } 
+                if (((BR.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "r"; } 
+                if (((BN.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "n"; } 
+                if (((BB.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "b"; } 
+                if (((BP.bb>>i)&1)==1) { this.charBoard[i / 8, i % 8] = "p"; } 
 
-                if (String.IsNullOrEmpty(chessBoard[i / 8, i % 8])){chessBoard[i / 8, i % 8] = " ";}
+                if (String.IsNullOrEmpty(this.charBoard[i / 8, i % 8])){this.hessBoard[i / 8, i % 8] = " ";}
             }
-            printChessBoard(chessBoard);
-
-            for(int i = 0; i < 64; i++)
-            {
-                Console.WriteLine("Iteration: " + i.ToString());
-                intToBoard(Bishop.movesLookUp[i]);
-
-            }
+            this.printCharBoard();
         }
-        public static void printChessBoard<T>(T[,] chessBoard)
+
+        /// <summary>
+        /// Prints graphical rep of the board in the console
+        /// </summary>
+        public void printCharBoard()
         {
-            for (int i = 0; i < chessBoard.GetLength(0); i++)
+            for (int i = 0; i < this.charBoard.GetLength(0); i++)
             {
-                for (int j = 0; j < chessBoard.GetLength(1); j++)
+                for (int j = 0; j < this.charBoard.GetLength(1); j++)
                 {
-                    Console.Write(chessBoard[i,j] + ",");
+                    Console.Write(this.charBoard[i,j] + ",");
                 }
                 Console.WriteLine();
             }
         }
 
-        public static void intToBoard(long bb)
+        /// <summary>
+        /// Prints graphical rep of a bitboard in the console
+        /// </summary>
+        public static void printBitBoard(long bb)
         {
+            string stringBoard = new String;
             if(bb < 0)
             {
                 stringBoard = Convert.ToString(bb, 2);  
@@ -173,6 +173,9 @@ namespace Chess
             }
         }
 
+        /// <summary>
+        /// Prints graphical rep of a bitboard in the console
+        /// </summary>
         public static string Reverse( string s )
         {
             char[] charArray = s.ToCharArray();
